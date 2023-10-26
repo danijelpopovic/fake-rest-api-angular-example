700455dfef0ebe91d0ea62beddf534c5a599e1ab
danijelpopovic <d.popovic89@gmail.com>
2023-10-26 23:26:27 +0200

    err

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
-import {ApiService} from "../service/api.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ApiService } from "../service/api.service";
 
 @Component({
   selector: 'app-login',
@@ -22,15 +22,18 @@ export class LoginComponent implements OnInit {
       username: this.loginForm.controls.username.value,
       password: this.loginForm.controls.password.value
     }
-    this.apiService.login(loginPayload).subscribe(data => {
-      debugger;
-      if(data.status === 200) {
-        window.localStorage.setItem('token', data.result.token);
-        this.router.navigate(['list-user']);
-      }else {
-        this.invalidLogin = true;
-        alert(data.message);
-      }
+    this.apiService.login(loginPayload).subscribe({
+      next: (data) => {
+        debugger;
+        if (data.status === 200) {
+          window.localStorage.setItem('token', data.result.token);
+          this.router.navigate(['list-user']);
+        } else {
+          this.invalidLogin = true;
+          alert(data.message);
+        }
+      },
+      error: (err) => { alert(err.message); }
     });
   }
 
